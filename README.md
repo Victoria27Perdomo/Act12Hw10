7e32adea417e28b38afa55b4aa07e6e073c2e55c
VICTORIA PERDOMO <victoria27perdomo@gmail.com>
2023-05-04 02:02:34 -0600

    Task10

--- README.md
+++ README.md
@@ -30,3 +30,40 @@ Create a Vue application where you can shape the data provided in the previous f
 4. Create the git project and upload the results to GitHub, to the project named "ChuckJokesVue".
 
 Remember to edit your README.md file with the description of the activity, take screenshots and attach everything in a document in report format along with the GitHub URL.
+
+##Homework 10
+
+After activity 12, we will modify the data as follows:
+
+```
+chuck: [ 
+    { 
+        "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png", 
+        "value": "Chuck Norris can skydive into outer space." 
+    }, 
+    { 
+        "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png", 
+        "value": "The chief export of Chuck Norris is pain." 
+    }, 
+    { 
+        "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png", 
+        "value": "Chuck Norris doesn't read books. He stares them down until he gets the information he wants." 
+    }, 
+    { 
+        "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png", 
+        "value": "Time waits for no man. Unless that man is Chuck Norris." 
+    }, 
+    { 
+        "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png", 
+        "value": "If you spell Chuck Norris in Scrabble, you win. Forever." 
+    }, 
+], 
+```
+
+Then, do the following:
+
+1. Create a component in Vue called <'chuck-card'> and take as template the code of a Bootstrap card with image and text.
+2. Use props to pass icon_url and values between components.
+3. Three cards should appear per line (two cards for the second line).
+4. Use Bootstrap classes to make the page responsive (on small screens, one card per line should be displayed).
+5. Update your project on GitHub by naming the commit "Task10".
--- main.js
+++ main.js
@@ -1,9 +1,14 @@
-Vue.component('chuck-jokes', {
-    props: ['todo'],
+Vue.component('chuck-card', {
+    props: ['image', 'title'],
     template:
     `
-        <div class="col">
-            <li class="lista">{{todo.value}}</li>
+        <div class="col">    
+            <div class="card">
+                <img class="card-img-top imgCard" v-bind:src="image" v-bind:alt="title">
+                <div class="card-body">
+                    <p class="card-text">{{title}}</p>
+                </div>
+            </div>
         </div>
     `
 });
@@ -12,21 +17,26 @@ var app = new Vue({
     el: '#app',
     data: {
         chuck: [ 
-            { 
+            {
+                icon_url: "https://img.europapress.es/fotoweb/fotonoticia_20150310130850-732359_420.jpg",
                 value: "Chuck Norris can skydive into outer space."
             }, 
-            {
-                value: "The chief export of Chuck Norris is pain."
-            }, 
             { 
+                icon_url: "https://movieplayer.net-cdn.it/t/images/2003/08/12/chuck-norris-22803_jpg_750x400_crop_q85.jpg",
+                value: "The chief export of Chuck Norris is pain."
+            },
+            {
+                icon_url: "https://images.uncyc.org/commons/5/5c/Chuck_Norris.jpg",
                 value: "Chuck Norris doesn't read books. He stares them down until he gets the information he wants."
             },
-            { 
+            {
+                icon_url: "https://londonfilmandcomiccon.com/wp-content/uploads/2023/03/ChuckNorrisWeb.png",
                 value: "Time waits for no man. Unless that man is Chuck Norris."
             }, 
-            {
+            { 
+                icon_url: "https://www.giantbomb.com/a/uploads/scale_medium/46/462814/3221502-8667199912-d2d02.jpg",
                 value: "If you spell Chuck Norris in Scrabble, you win. Forever."
-            } 
+            }, 
         ] 
     }
 });
